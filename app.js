1b19ee17b8c0ffb061d4a5ad7e08988cb9fe287d
JpunktWpunkt <wittig.jessica@googlemail.com>
2022-05-01 19:34:30 +0200

    change lokal user and add async function for prepare random picker

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const app = Vue.createApp({
             picture: 'https://randomuser.me/api/portraits/men/10.jpg',
         }
     },
+    methods: {
+        async getUser() {
+            const response = await fetch('https://randomuser.me/api')
+            const {results} = await response.json()
+
+            console.log(results)
+            this.firstName = 'Sam'
+            this.lastName = 'Doe'
+            this.email = 'sam@gmail.com'
+            this.gender = 'female'
+            this.picture = 'https://randomuser.me/api/portraits/women/10.jpg'
+        },
+    },
 })
 
 app.mount('#app')
